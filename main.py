8a12e824145c3e83e6f562e1b7f65fe823a2ddb9
Beamma <17141@burnside.school.nz>
2021-06-09 22:46:11 +1200

    V2.2 Beggining of logon

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template, redirect, url_for, request
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import insert
 from config import Config
-from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 app.config.from_object(Config)  # applying all config to app
@@ -13,13 +12,20 @@ db = SQLAlchemy(app)
 import models
 @app.route('/', methods=['GET', 'POST'])
 def home():
-    result = models.User.query.filter_by(id=1).all()
+    result = models.User.query.all()
     print(result)
     return render_template("home.html", result = result)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template("login.html")
+    if request.method == "POST":
+
+        user_name = request.form.get("user_name")
+        password = models.check_password(request.form.get("password"))
+        print(password)
+        return render_template("login.html")
+    else:
+        return render_template("login.html")
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -27,10 +33,11 @@ def register():
 
         # Retreive User And Password, Hash Password
         user_name = request.form.get("user_name")
-        hashed_password = generate_password_hash(request.form.get("password"), salt_length=10)
         email = request.form.get("email")
-        print(user_name, hashed_password, email) # DEBUG
-        user.insert().values(name=user_name, email=email, pass_word=hashed_password, ird=0, bank=0, card=0, adress=0, balance=0)
+        user = models.User(name=user_name, email=email, ird='0', address='0', bank='0', card='0', balance='0')
+        user.set_password(request.form.get("password"))
+        db.session.add(user)
+        db.session.commit()
 
         return redirect(url_for('home'))
     else:
--- models.py
+++ models.py
@@ -1,41 +1,24 @@
 from main import db
-#
-#
-# PizzaTopping = db.Table('PizzaTopping', db.Model.metadata,
-#                     db.Column('Pizza_id', db.Integer, db.ForeignKey('Pizza.id')),
-#                     db.Column('Topping_id', db.Integer, db.ForeignKey('Topping.id'))
-#                    )
-#
-#
-# class Pizza(db.Model):
-#   __tablename__ = 'Pizza'
-#   id = db.Column(db.Integer, primary_key=True)
-#   name = db.Column(db.String())
-#   description = db.Column(db.String())
-#
-#   toppings = db.relationship('Topping', secondary=PizzaTopping, back_populates='pizzas')
-#
-#
-# class Topping(db.Model):
-#   __tablename__ = 'Topping'
-#   id = db.Column(db.Integer, primary_key=True)
-#   name = db.Column(db.String())
-#   description = db.Column(db.String())
-#
-#   pizzas = db.relationship('Pizza', secondary=PizzaTopping, back_populates='toppings')
+from werkzeug.security import generate_password_hash, check_password_hash
 
 class User(db.Model):
     __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True, nullable=False)
     name = db.Column(db.String(), nullable=False)
     email = db.Column(db.String(), nullable=False)
-    pass_word = db.Column(db.String(), nullable=False)
+    password = db.Column(db.String(), nullable=False)
     ird = db.Column(db.Integer, nullable=False)
     address = db.Column(db.String(), nullable=False)
     bank = db.Column(db.Integer, nullable=False, default=0)
     card = db.Column(db.Integer, nullable=False, default=0)
     balance = db.Column(db.Integer, nullable=False, default=0)
 
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
+
     def __repr__(self):
-        return f'<User {self.name}>'
+        return f'<User {self.id, self.name, self.email, self.password, self.ird, self.address, self.bank, self.card, self.balance}>'
 # db.create_all(extend_existing=True)
