c98143502c5960e5229d7f109cbf0bfda410b725
Joel Bremner <jbr257@uclive.ac.nz>
2024-08-21 23:53:04 +1200

    Working to fix this project

--- forms.py
+++ forms.py
@@ -1,7 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import IntegerField, TextField, TextAreaField, SelectField, StringField, PasswordField, RadioField, DateField
-from wtforms.widgets import PasswordInput, html5
-from wtforms.fields import html5 as h5fields
+from wtforms import IntegerField, StringField, TextAreaField, SelectField, PasswordField, RadioField, DateField
+from wtforms.widgets import PasswordInput, NumberInput
 from wtforms.validators import DataRequired, Optional, ValidationError, Email
 import models
 from datetime import datetime
@@ -10,29 +9,30 @@ class Register(FlaskForm):
 
     def check_email(form, field):
         user = models.User.query.filter_by(email=field.data).first()
-        if user != None:
+        if user is not None:
             raise ValidationError("Email Already Used")
 
-    user_name = TextField("user_name", validators=[DataRequired()])
+    user_name = StringField("user_name", validators=[DataRequired()])
     password = PasswordField("password", validators=[DataRequired()])
     email = StringField("email", validators=[DataRequired(), Email(), check_email])
 
 
 class Login(FlaskForm):
 
-    def exisiting_email(form, field):
+    def existing_email(form, field):
         user = models.User.query.filter_by(email=field.data).first()
         if user is None:
             raise ValidationError("There Is No Account For This Email")
 
     password = PasswordField("password", validators=[DataRequired()])
-    email = StringField("email", validators=[DataRequired(), Email(), exisiting_email])
+    email = StringField("email", validators=[DataRequired(), Email(), existing_email])
 
 
 class Trade(FlaskForm):
 
     trade = SelectField("trade", validators=[DataRequired()])
-    amount = h5fields.IntegerField("amount", widget=html5.NumberInput(), validators=[DataRequired()])
+    amount = IntegerField("amount", widget=NumberInput(), validators=[DataRequired()])
+
 
 class Stock(FlaskForm):
 
@@ -41,7 +41,7 @@ class Stock(FlaskForm):
 
 class All_Stock(FlaskForm):
 
-    search = TextField("search")
+    search = StringField("search")
     type = RadioField("type", choices=[("All", "All"), ("Company", "Company"), ("ETF", "ETF"), ("Fund", "Fund")], default="All", validators=[DataRequired()])
     category = RadioField("category", choices=[("All", "All"), ("Tech", "Tech"), ("Agriculture", "Agriculture"), ("Retail", "Retail"), ("Finance", "Finance"), ("Automotive", "Automotive")], default="All", validators=[DataRequired()])
     exchange = RadioField("exchange", choices=[("All", "All"), ("NASDAQ", "NASDAQ"), ("NYSE", "NYSE")], default="All", validators=[DataRequired()])
@@ -49,6 +49,6 @@ class All_Stock(FlaskForm):
 
 class Deposit(FlaskForm):
 
-    money = IntegerField("money", widget=html5.NumberInput(), validators=[DataRequired()])
-    card = h5fields.IntegerField("card", widget=html5.NumberInput(), validators=[DataRequired()])
-    cvv = h5fields.IntegerField("cvv", widget=html5.NumberInput(), validators=[DataRequired()])
+    money = IntegerField("money", widget=NumberInput(), validators=[DataRequired()])
+    card = IntegerField("card", widget=NumberInput(), validators=[DataRequired()])
+    cvv = IntegerField("cvv", widget=NumberInput(), validators=[DataRequired()])
